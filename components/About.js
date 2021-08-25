4ae99b6949be267d678c988332ccbe652b5dcd27
Your Name <you@example.com>
2021-08-25 07:41:01 +0300

    kkfkf

---
+++ components/About.js
@@ -0,0 +1,18 @@
+import styles from '../styles/About.module.css';
+
+
+function About() {
+    return (
+        <>
+         <div className={styles.about}>
+             <h2>Company Profile</h2>
+             <div>
+             MGrinders Technologies was formed in August 2021 to take advantage of increasing opportunities in the Information Communication Technology (ICT) industry.
+             </div>
+
+             </div>   
+        </>
+    )
+}
+
+export default About
---
+++ components/Banner.js
@@ -0,0 +1,15 @@
+
+
+import styles from '../styles/Banner.module.css'
+function Banner({im}) {
+    return (
+        <div className={styles.kk} id="kk" >
+            <div className={styles.spacer}/>
+            <div className={styles.text}><h2>About Us</h2></div>
+            
+           
+        </div>
+    )
+}
+
+export default Banner
