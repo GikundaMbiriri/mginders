c700adaacc26e077aa6bf959aa7f1a05c17c85db
Your Name <you@example.com>
2021-09-08 19:45:40 +0300

    kjkdj

--- components/Nav.js
+++ components/Nav.js
@@ -1,58 +1,58 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import {useState} from 'react'
+import { useState } from 'react'
 import styles from '../styles/nav.module.css'
-
+import gh from '../public/daro.jpg'
 function Nav() {
-  const [ham,setHam]=useState('hamburger');
-  const [navl,setNavl]=useState('nav-links');
-const [nl,setNl]=useState('l');
-    function handleClicks() {
-    if (navl==='nav-links') {
+  const [ham, setHam] = useState('hamburger');
+  const [navl, setNavl] = useState('nav-links');
+  const [nl, setNl] = useState('l');
+  function handleClicks() {
+    if (navl === 'nav-links') {
       setNavl('nav-links open')
     }
-    else{
+    else {
       setNavl('nav-links')
     }
-      if (nl==='l') {
+    if (nl === 'l') {
       setNl('fade')
     }
-    else{
+    else {
       setNl('')
     }
-      if (ham==='hamburger') {
+    if (ham === 'hamburger') {
       setHam('hamburger toggle')
     }
-    else{
-  setHam('hamburger')
+    else {
+      setHam('hamburger')
     }
   }
   console.log(ham)
   console.log(navl)
   console.log(nl)
-    return (
- <>
-             <nav>
+  return (
+    <>
+      <nav>
         <div className="logo">
-            <img src="logo.svg" alt="Logo Image"/>
+          <img src={gh} alt="Logo Image" />
         </div>
         <div className={ham} onClick={handleClicks}>
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
         </div>
         <ul className={navl}>
-            <li className={nl}><a className="on" href="#">Home</a></li>
-            <li className={nl}><a href="#">Solutions</a></li>
-            <li className={nl}><a href="#">Products</a></li>
-            <li className={nl}><a href="#">Services</a></li>
-            <li className={nl}><a href="#">Contact Us</a></li>
-            <li className={nl}><button className="login-button" href="#">Login</button></li>
-            <li className={nl}><button className="join-button" href="#">Join</button></li>
+          <li className={nl}><a className="on" >Home</a></li>
+          <li className={nl}><a >Solutions</a></li>
+          <li className={nl}><a >Products</a></li>
+          <li className={nl}><a >Services</a></li>
+          <li className={nl}><a >Contact Us</a></li>
+          <li className={nl}><button className="login-button" >Login</button></li>
+          <li className={nl}><button className="join-button" >Join</button></li>
         </ul>
-    </nav>
-       </>
-    )
+      </nav>
+    </>
+  )
 }
 
 export default Nav
